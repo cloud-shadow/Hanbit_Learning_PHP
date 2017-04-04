26d9cd6b17f09b7c04723f7ed64479c579ed17f2
cloudshadow <cloudshadow@gmail.com>
2017-04-04 14:35:09 +0900

    display-code 13장 한글화

--- display-code/13/IsolateValidationTest.php
+++ display-code/13/IsolateValidationTest.php
@@ -1,4 +1,4 @@
-// validate_form() is defined in this file
+// validate_form()이 정의된 파일
 include 'isolate-validation.php';
 
 class IsolateValidationTest extends PHPUnit_Framework_TestCase {
@@ -6,36 +6,35 @@ class IsolateValidationTest extends PHPUnit_Framework_TestCase {
     public function testDecimalAgeNotValid() {
         $submitted = array('age' => '6.7',
                            'price' => '100',
-                           'name' => 'Julia');
+                           'name' => '김영희');
         list($errors, $input) = validate_form($submitted);
-        // Expecting only one error -- about age
-        $this->assertContains('Please enter a valid age.', $errors);
+        // 나이에 관련된 오류만 확인한다.
+        $this->assertContains('나이를 정확하게 입력해주세요.', $errors);
         $this->assertCount(1, $errors);
     }
 
     public function testDollarSignPriceNotValid() {
         $submitted = array('age' => '6',
                            'price' => '$52',
-                           'name' => 'Julia');
+                           'name' => '김영희');
         list($errors, $input) = validate_form($submitted);
-        // Expecting only one error -- about age
-        $this->assertContains('Please enter a valid price.', $errors);
+        // 가격에 관련된 오류만 확인한다.
+        $this->assertContains('가격을 정확하게 입력해주세요.', $errors);
         $this->assertCount(1, $errors);
     }
 
     public function testValidDataOK() {
         $submitted = array('age' => '15',
                            'price' => '39.95',
-                           // Some whitespace around name that
-                           // should be trimmed
-                           'name' => '  Julia  ');
+        // 이름 양 끝에 화이트스페이스가 있으면 제거된다.
+                           'name' => ' 김영희 ');
         list($errors, $input) = validate_form($submitted);
-        // Expecting no errors
+        // 오류가 없어야 한다.
         $this->assertCount(0, $errors);
-        // Expecting 3 things in input
+        // $input 배열의 세 가지 값을 확인한다.
         $this->assertCount(3, $input);
         $this->assertSame(15, $input['age']);
         $this->assertSame(39.95, $input['price']);
-        $this->assertSame('Julia', $input['name']);
+        $this->assertSame('김영희', $input['name']);
     }
 }
