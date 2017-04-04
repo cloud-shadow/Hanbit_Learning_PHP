2ef2b4b6269c508f526b8362b2b2a28952e88a4b
cloudshadow <cloudshadow@gmail.com>
2017-04-04 14:29:10 +0900

    16장 한글화

--- code/16/vendor.php
+++ code/16/vendor.php
@@ -1,23 +1,22 @@
 <?php
 
-// Tell PHP to load Composer's class-finding logic
+// 컴포저의 클래스 검색 로직 사용하기.
 require 'vendor/autoload.php';
 
-// The Swift_Message class is automatically available without
-// any additional work
+// Swift_Message 클래스를 자동으로 불러오므로 즉시 사용할 수 있다.
 $message = Swift_Message::newInstance();
 $message->setFrom('julia@example.com');
 $message->setTo(array('james@example.com' => 'James Beard'));
-$message->setSubject('Delicious New Recipe');
+$message->setSubject('건강한 간식 레시피');
 $message->setBody(<<<_TEXT_
-Dear James,
+아들,
 
-You should try this: puree 1 pound of chicken with two pounds
-of asparagus in the blender, then drop small balls of the mixture
-into a deep fryer. Yummy!
+출출할 때는 이렇게 해보렴. 닭가슴살 1인분과 아스파라거스 몇 개를
+믹서기에 넣고 함께 갈아서 경단처럼 뭉친 다음 달궈진 프라이팬에 넣고 볶으면
+훌륭한 요리가 된단다. 맛있을거야!
 
-Love,
-Julia
+사랑을 담아,
+엄마가.
 
 _TEXT_
 );
